b1f4a13426ed8b0751acbcc9044268d0ab6f1297
Brendan Mangan <39705700+beemangan@users.noreply.github.com>
2022-11-09 09:34:34 -0500

    New dictionaries (Currently not in use)

--- source/dictionaries.py
+++ source/dictionaries.py
@@ -1,32 +1,7 @@
-FileNameDict = {
-    '173': 'SpinUp',
-    '154' : 'TippingPoint',
-    '139' : 'ChangeUp',
-    '130' : 'TowerTakeover',
-    '125' : 'TurningPoint',
-    '119' : 'InTheZone',
-    '115' : 'Starstruck',
-    '110' : 'NothingButNet', 
-    '102' : 'Skyrise',
-    '92' : 'TossUp',
-    '85' : 'SackAttack', 
-    '73' : 'Gateway', 
-    '7' : 'RoundUp', 
-    '1' : 'CleanSweep'
-    }
-SeasonNameDict = {
-    '173': 'Spin Up',
-    '154' : 'Tipping Point',
-    '139' : 'Change Up',
-    '130' : 'Tower Takeover',
-    '125' : 'Turning Point',
-    '119' : 'In The Zone',
-    '115' : 'Starstruck',
-    '110' : 'Nothing But Net', 
-    '102' : 'Skyrise',
-    '92' : 'Toss Up',
-    '85' : 'Sack Attack', 
-    '73' : 'Gateway', 
-    '7' : 'Round Up', 
-    '1' : 'Clean Sweep'
-    }
+import pandas as pd
+
+seasons = pd.io.parquet.read_parquet(r'C:\Users\Brendan\Documents\Programming\Python Testing\robodata\source\parquet\seasons.parquet',engine='pyarrow')
+sdict = dict(zip(seasons.id,seasons.name)) ## Season Dictionary {id:name}
+
+teams = pd.io.parquet.read_parquet(r'C:\Users\Brendan\Documents\Programming\Python Testing\robodata\source\parquet\teams.parquet',engine='pyarrow')
+tdict = dict(zip(teams.id,teams.number)) ## Team Dictionary {id:number}
